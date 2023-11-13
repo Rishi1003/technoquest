308ad20d0568fffc26167046081abf681c55bd1a
Rishi Karanam <rshkrnm@gmail.com>
2023-11-13 20:47:16 +0530

    fsad

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Hero from "./Components/Hero"
 import Navbar from "./components/Navbar"
 import Intro from './Components/Intro'
 import Steps from "./Components/Steps"
+import Tracks from "./Components/Tracks"
+import Choose from "./Components/Choose"
 
 export default function App() {
   return (
-    <main className="bg-black">
+    <main className="bg-black overflow-x-hidden flex flex-col items-center">
       <Navbar/>
       <Hero/>
       <Intro/> 
       <Steps/>
+      <Choose/>
+      <Tracks/>
     </main>
   )
 }
