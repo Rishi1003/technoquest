f215ce3d5170d5c98951df0518fcdb341e9ff992
Rishi Karanam <rshkrnm@gmail.com>
2024-05-31 19:31:55 +0530

    added terms and conds

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import TermsAndConditions from './tnc.jsx';
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+        path:"/terms",
+        element:<TermsAndConditions/>
+    }
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <RouterProvider router={router} />
 
 )
